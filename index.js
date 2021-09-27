4be419be4042ed9dc9fb913b502d8481004c0372
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-09-27 21:37:30 +0200

    yeaa

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("YEAA");
+
+const https = require('https');
+
+
+const vReq = (reqCfg) => {
+  this.data = (typeof reqCfg.data !== 'undefined') ? reqCfg.data : "";
+  this.options = (typeof reqCfg.options !== 'undefined') ? reqCfg.options : "";
+
+  this.req = https.request(this.options, res => {
+    this.postReqInfo(res);
+    /*res.on('data', d => {
+      process.stdout.write(d)
+    })*/
+  })
+
+  this.postReqInfo = (res) => {
+    console.log(`\nHost:  ${res.socket._host}`)
+    console.log(`Status Code: ${res.statusCode}`)
+    console.log(`Path: ${res.req.path}`)
+    console.log(`Method: ${res.req.method}`)
+    //console.log(res.req)
+    console.log(`Port: ${res.req.agent.defaultPort}`)
+    console.log(`keepAliveMsecs: ${res.req.agent.keepAliveMsecs}`)
+    console.log(`keepAlive: ${res.req.agent.keepAlive}`)
+    //console.log(`maxCachedSessions: ${res.req.agent.maxCachedSessions}`)
+  }
+
+  this.req.on('error', error => {
+    console.error(error)
+  })
+
+  this.req.write(this.data)
+  this.req.end()
+}
+
+
+
+module.exports = vReq;
